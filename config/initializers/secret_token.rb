dde9339cc4fd277538f7413bc04d7ed4cc3e59e7
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2013-11-04 10:13:34 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FauxTwitter::Application.config.secret_token = '1348a3cd4442c13f6aa98923aa37db1e3aa8d765bfb2106d09ff00f9a05b6657751a13f9c848ccabfd9cf49c247c38d392af6d3f40667d970743f3fb512d434e'
